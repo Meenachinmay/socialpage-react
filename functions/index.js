a68a58d13cb94d9a610854b719b0ecefdc314782
meenachinmay <chinmayanand896@gmail.com>
2020-07-20 19:14:13 +0530

    all the cloud functions are working excepting image upload

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,50 @@
 const functions = require('firebase-functions');
 
-const app = require('express')();
+const admin = require('firebase-admin');
 
-const { getAllScreams, createNewScream } = require('./handlers/screams');
-const { signup, login} = require('./handlers/users');
-const { admin } = require('./utils/admin');
+const app = require('express')();
+admin.initializeApp(functions.config().firebase);
+
+const config = {
+    apiKey: "AIzaSyCGntR4Sm4y960n46VUygtJwONMMVXAiWo",
+    authDomain: "socialpage-react.firebaseapp.com",
+    databaseURL: "https://socialpage-react.firebaseio.com",
+    projectId: "socialpage-react",
+    storageBucket: "socialpage-react.appspot.com",
+    messagingSenderId: "733309548610",
+    appId: "1:733309548610:web:c0d9d9b2a6996874bed5db",
+    measurementId: "G-CR05RZ2RZP"
+  };
+
+const firebase = require('firebase');
+firebase.initializeApp(config)
+
+const db = admin.firestore();
 
 //　もう作ったスーキルムを探して、もらうべく
-app.get('/screams', getAllScreams);
+app.get('/screams', (req, res) => {
+    db
+    .collection('screams')
+    .orderBy('createdAt','desc')
+    .get()
+    .then(data => {
+        let screams = [];
+        data.forEach(doc => {
+            screams.push({
+                screamId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt,
+                commentCount: doc.data().commentCount,
+                likeCount: doc.data().likeCount,
+            });
+        });
+        return res.json(screams);
+    })    
+    .catch(err => console.log(err));
+});
 
+// Writing a middleware
 const FBAuth = (req, res, next) => {
     let idToken;
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
@@ -21,7 +57,6 @@ const FBAuth = (req, res, next) => {
     admin.auth().verifyIdToken(idToken)
         .then(decodedToken => {
             req.user = decodedToken;
-            console.log(decodedToken);
             return db.collection('users')
                 .where('userId', '==', req.user.uid)
                 .limit(1)
@@ -31,11 +66,12 @@ const FBAuth = (req, res, next) => {
             req.user.handle = data.docs[0].data().handle;
             return next();
         })
-        .catch(error => {
-            console.log('Error while verifying token ',error);
-            return res.status(403).json({ error, message: "hello world" });
+        .catch(err => {
+            console.log('Error while verifying token ',err);
+            return res.status(403).json({ err });
         });
-};
+}
+
 
 //  新しいスーキルムを作るべく
 app.post('/new-scream',FBAuth, (req, res) => {
@@ -46,20 +82,164 @@ app.post('/new-scream',FBAuth, (req, res) => {
     }
 
     db
-    .collection('screams')
-    .add(newScream)
-    .then(doc => {
-        res.json({ message: `ドキュメント ${doc.id} を作成させていただきました。`});
-    })
-    .catch(err => {
-        res.status(500).json({ error: `何かがうまくいきませんでした。`});
-        console.error(err);
-    }); 
+        .collection('screams')
+        .add(newScream)
+        .then(doc => {
+            res.json({ message: `ドキュメント ${doc.id} を作成させていただきました。`});
+        })
+        .catch(err => {
+            res.status(500).json({ error: `何かがうまくいきませんでした。`});
+            console.error(err);
+        }); 
 });
 
+    const isEmail = (email) => {
+        const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (email.match(emailRegEx)) return true;
+        else return false;
+    }
+
+    const isEmpty = (string) => {
+        if (string.trim() === '') return true;
+        else return false;
+    }
+
 // Signup Route
-app.post('/signup', signup);
+app.post('/signup', (req,res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    let errors = {};
+
+    if (isEmpty(newUser.email)){
+        errors.email = 'Must not be empty'
+    }else if (!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address'
+    }
+
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    const noImg = 'no-img.png';
+    
+    // TODO validate data
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if (doc.exists){
+                return res.status(400).json({ handle: 'this handle is already taken by other user' });
+            }else {
+                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then(idToken => {
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
+                userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({token});
+        })
+        .catch(error => {
+            console.error(error);
+            return res.status(500).json({error: err.code});
+        });
+});
+
 // Login route
-app.post('/login', login);
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if (isEmpty(user.email)) errors.email = 'Must not empty';
+    if (isEmpty(user.password)) errors.password = 'Must not empty';
+
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return res.json({token});
+        })
+        .catch(err => {
+            console.error(err);
+            if (err.code === 'auth/wrong-password'){
+                return res.status(403).json({general: 'Wrong credentials, please try again with correct email and password'});
+            }else return res.status(500).json({ error: err.code });
+        })
+});
+
+
+app.post('/user/image', FBAuth,(req, res) => {
+    const BusBoy = require('busboy');
+    const path = require('path');
+    const os = require('os');
+    const fs = require('fs');
+
+    const busboy = new BusBoy({ headers: req.headers });
+
+    let imageFilename;
+    let imageToBeUploaded = {};
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        
+        if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+            return res.status(400).json({error: 'Wrong file type posted'});
+        }
+
+        const imageExtension = filename.split('.')[filename.split('.').length - 1];
+        
+        imageFilename = `${Math.round(Math.random() * 100000000000)}.${imageExtension}`;
+        const filePath = path.join(os.tmpdir(), imageFilename);
+        imageToBeUploaded = { filePath, mimetype };
+        file.pipe(fs.createWriteStream(filepath));
+    });
+
+    busboy.on('finish', () => {
+        admin.storage().bucket(config.storageBucket).upload(imageToBeUploaded.filePath, {
+            resumable: false,
+            metadata: {
+                metadata: {
+                    contentType: imageToBeUploaded.mimetype
+                }
+            }
+        })
+        .then(() => {
+            const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFilename}?alt=media`;
+            return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+        })
+        .then(() => {
+            return res.json({ message: 'Image uploaded successfully' });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(403).json({ error: err.code });
+        });
+    });
+    busboy.end(req.rawBody);
+});
 
 exports.api = functions.region('us-central1').https.onRequest(app);
